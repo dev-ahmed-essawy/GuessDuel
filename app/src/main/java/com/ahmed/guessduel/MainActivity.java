a4fda64cb97d81a53b8d891e3fc704cf02ea74af
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 01:08:02 +0300

    Change layout to use activity_main XML

--- app/src/main/java/com/ahmed/guessduel/MainActivity.java
+++ app/src/main/java/com/ahmed/guessduel/MainActivity.java
@@ -2,18 +2,11 @@ package com.ahmed.guessduel;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView tv = new TextView(this);
-        tv.setText("Hello Ahmed ✅");
-        tv.setTextSize(24);
-
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
     }
 }
