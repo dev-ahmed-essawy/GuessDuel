33654011841154b57e607397708840ef13c330e6
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 00:36:16 +0300

    Add MainActivity with greeting message

---
+++ app/src/main/java/com/ahmed/guessduel/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ahmed.guessduel;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello Ahmed ✅");
+        tv.setTextSize(24);
+
+        setContentView(tv);
+    }
+}
