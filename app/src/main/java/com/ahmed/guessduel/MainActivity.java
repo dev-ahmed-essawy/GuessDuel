96058ce6cf7d7c1276580b352ff084dc0f5df1f1
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 01:22:34 +0300

    Add button click to start GameActivity

--- app/src/main/java/com/ahmed/guessduel/MainActivity.java
+++ app/src/main/java/com/ahmed/guessduel/MainActivity.java
@@ -1,12 +1,23 @@
 package com.ahmed.guessduel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.startBtn);
+
+        start.setOnClickListener(v -> {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        });
     }
 }
+``
