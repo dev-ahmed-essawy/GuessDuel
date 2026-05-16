d9aff2ad44f85b52db49b958cc26e1b8f9e3f5c2
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-16 20:46:52 +0300

    Add files via upload

---
+++ app/src/main/com/essawy/guessduel/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.guessduel;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.startBtn);
+        start.setOnClickListener(v ->
+                startActivity(new Intent(this, GameActivity.class))
+        );
+    }
+}
