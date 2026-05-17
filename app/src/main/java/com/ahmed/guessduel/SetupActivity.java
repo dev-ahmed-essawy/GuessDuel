6e4d59f41ad8eacf61780103b2c2d29968574ccd
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 06:10:12 +0300

    Change SetupActivity to extend Activity instead of AppCompatActivity

--- app/src/main/java/com/ahmed/guessduel/SetupActivity.java
+++ app/src/main/java/com/ahmed/guessduel/SetupActivity.java
@@ -1,11 +1,11 @@
 package com.ahmed.guessduel;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.*;
 
-public class SetupActivity extends AppCompatActivity {
+public class SetupActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
