fbed34aec71a0924ed1110838101d263a5f36454
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 23:16:14 +0300

    Increase splash screen duration to 3 seconds

---
+++ app/src/main/java/com/ahmed/guessduel/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.ahmed.guessduel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, ModeActivity.class);
+            startActivity(i);
+            finish();
+        }, 3000); // 3 sec
+    }
+}
