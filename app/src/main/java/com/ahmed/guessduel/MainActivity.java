1817d406f57ce18c1ba61a8dafe2166ea9a5e9ce
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 06:09:19 +0300

    Change MainActivity to extend Activity instead of AppCompatActivity

--- app/src/main/java/com/ahmed/guessduel/MainActivity.java
+++ app/src/main/java/com/ahmed/guessduel/MainActivity.java
@@ -1,11 +1,11 @@
 package com.ahmed.guessduel;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
