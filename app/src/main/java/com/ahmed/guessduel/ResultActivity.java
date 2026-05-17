03caf9d014ffda31c95d252cdcdb7c05325231d7
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 06:09:47 +0300

    Change ResultActivity to extend Activity instead of AppCompatActivity

--- app/src/main/java/com/ahmed/guessduel/ResultActivity.java
+++ app/src/main/java/com/ahmed/guessduel/ResultActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
