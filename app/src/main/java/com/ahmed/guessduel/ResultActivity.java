2bdedfaa3aeb92367f9310c808757e7b97cf1643
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-17 06:30:17 +0300

    Fix package declaration and import statement

--- app/src/main/java/com/ahmed/guessduel/ResultActivity.java
+++ app/src/main/java/com/ahmed/guessduel/ResultActivity.java
@@ -1,4 +1,6 @@
-package com.ahmed.guessduel;package com.import android.app.Activity;
+package com.ahmed.guessduel;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
