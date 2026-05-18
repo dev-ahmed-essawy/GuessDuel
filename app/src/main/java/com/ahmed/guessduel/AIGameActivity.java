dad35cfe4684509fa1bde1381afd2f592a02b4b9
Ahmed Abdelmonem <dev.ahmed.essawy@gmail.com>
2026-05-19 00:45:47 +0300

    Add Intent import to AIGameActivity

--- app/src/main/java/com/ahmed/guessduel/AIGameActivity.java
+++ app/src/main/java/com/ahmed/guessduel/AIGameActivity.java
@@ -3,6 +3,7 @@ package com.ahmed.guessduel;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.content.Intent;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
